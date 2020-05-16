bf60139c69657fc895e3677287139f8b33c48d5f
Jose Diaz <jodia.ms@outlook.com>
2020-05-16 13:23:53 -0400

    created

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+
+$(function () {
+  let amenityDict = {};
+  $('input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenityDict[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenityDict[$(this).data('id')];
+    }
+    let amenityValues = Object.values(amenityDict);
+    if (amenityValues.length > 0) {
+      $('div.amenities > h4').text(amenityValues.join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+});
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(function () {
+  let amenityDict = {};
+  $('input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenityDict[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenityDict[$(this).data('id')];
+    }
+    let amenityValues = Object.values(amenityDict);
+    if (amenityValues.length > 0) {
+      $('div.amenities > h4').text(amenityValues.join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
