02e828ca1e5e326165065c395e557a5ce4359804
Jose Diaz <jodia.ms@outlook.com>
2020-05-18 23:36:08 -0400

    all

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,8 +3,8 @@
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
+from flask import Flask, url_for, render_template, make_response, jsonify
+from flask_cors import cross_origin, CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
